92c071949a4fea99231f735b9888ba6636abe969
TG <TG@172.16.137.184>
2023-11-09 15:38:40 +0700

    Change name: Customer to User

---
+++ src/CONTROLLER/Product.java
@@ -0,0 +1,35 @@
+package CONTROLLER;
+
+public abstract class Product {
+
+	// Fields
+    public String name;
+    private  int productID;
+    public double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
+}
